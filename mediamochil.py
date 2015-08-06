7bbba3c281839ab080816e4af5a7de2afdf06f4c
Hemanth Kumar <hemanth7787@gmail.com>
2015-08-07 01:36:13 +0530

    Seek function

--- mediamochil.py
+++ mediamochil.py
@@ -13,6 +13,7 @@ class MainWin(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWin, self).__init__(parent)
         self.setupUi(self)
+        self.debug = False
 
         headers = [self.tr("Title"), self.tr("Artist"), self.tr("Album"), self.tr("Year")]
         self.musicTable.setHorizontalHeaderLabels(headers)
@@ -21,12 +22,15 @@ class MainWin(QMainWindow, Ui_MainWindow):
         self.connect(self.musicTable, SIGNAL('cellPressed(int, int)'), self.tableClicked)
 
         self.volume.valueChanged.connect(self.volume_control)
+        self.horizontalSlider.setTickInterval(10)
+        self.horizontalSlider.setSingleStep(1)
 
         self.connect(self.open, SIGNAL("clicked()"), self.addFiles)
         self.dw_directory = "./"
         self.sources = []
         self.player = pyglet.media.Player()
         self.mediaLoad = None
+        self.connect(self.horizontalSlider, SIGNAL("sliderReleased()"), self.seek)
         self.connect(self.pause, SIGNAL("clicked()"), self.player.pause)
         self.connect(self.play, SIGNAL("clicked()"), self.player.play)
         self.connect(self.next_item, SIGNAL("clicked()"), self.player.next_source)
@@ -35,8 +39,26 @@ class MainWin(QMainWindow, Ui_MainWindow):
         #self.worker.start()
 
     def volume_control(self):
-        print " volume is now " + str(self.volume.value() + 1) + "%"
+        if self.debug:
+            print " volume is now " + str(self.volume.value() + 1) + "%"
         self.player.volume = float(self.volume.value() + 1)/100
+
+    def seek(self):
+        position = self.translate(self.horizontalSlider.value(), 0.00, 99.00, 0.00, self.mediaLoad.duration)
+        self.player.seek(position)
+        if self.debug:
+            print "seek " + str(position)
+
+    def translate(self, value, leftMin, leftMax, rightMin, rightMax):
+        # Figure out how 'wide' each range is
+        leftSpan = leftMax - leftMin
+        rightSpan = rightMax - rightMin
+
+        # Convert the left range into a 0-1 range (float)
+        valueScaled = float(value - leftMin) / float(leftSpan)
+
+        # Convert the 0-1 range into a value in the right range.
+        return rightMin + (valueScaled * rightSpan)
 
     def set_progress(self, progress):
         print progress
@@ -93,6 +115,7 @@ class MainWin(QMainWindow, Ui_MainWindow):
         for mediafile in files:
             self.mediaLoad = pyglet.media.load(mediafile)
             self.player.queue(self.mediaLoad)
+        print self.mediaLoad.duration
         self.player.play()
 
 
